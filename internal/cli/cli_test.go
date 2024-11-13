05f3e80c2be24edc002ab85e98d45e6d254ab23b
Holly Rees <holly.rees@bjss.com>
2024-11-13 12:44:01 +0000

    add cli tests following refactor

--- internal/cli/cli_test.go
+++ internal/cli/cli_test.go
@@ -11,7 +11,6 @@ import (
 // func TestListToDos(t *testing.T) {
 // }
 
-// this tests the conversion from string to ToDo
 func TestCliHandleAddToDo(t *testing.T) {
 	store := &storage.Inmemory{Todos: []models.ToDo{
 		{Task: "Task 1", Completed: false},
@@ -31,23 +30,59 @@ func TestCliHandleAddToDo(t *testing.T) {
 	}
 }
 
-// ask alan/oliver about this test - just testing what's already been tested in inmemory test?
-// func TestCliMarkComplete(t *testing.T) {
-// 	store := &storage.Inmemory{Todos: []models.ToDo{
-// 		{Task: "Task 1", Completed: false},
-// 	}}
-// 	app := App{Store: store}
+func TestCliHandleMarkComplete(t *testing.T) {
+	store := &storage.Inmemory{Todos: []models.ToDo{
+		{Task: "Task 1", Completed: false},
+	}}
+	app := App{Store: store}
 
-// 	expected := []models.ToDo{
-// 		{Task: "Task 1", Completed: true},
-// 	}
+	expected := []models.ToDo{
+		{Task: "Task 1", Completed: true},
+	}
 
-// 	app.MarkComplete(1)
-// 	actual := app.Store.GetTodos()
+	app.HandleMarkComplete("complete 1")
+	actual := app.Store.GetTodos()
 
-// 	if !slices.Equal(actual, expected) {
-// 		t.Errorf("Expected %v, got %v", expected, actual)
-// 	}
-// }
+	if !slices.Equal(actual, expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
 
-// tests for CLI interface? mock?
+func TestCliHandleDelete(t *testing.T) {
+	store := &storage.Inmemory{Todos: []models.ToDo{
+		{Task: "Task 1", Completed: false},
+		{Task: "Task 2", Completed: false},
+	}}
+	app := App{Store: store}
+
+	expected := []models.ToDo{
+		{Task: "Task 2", Completed: false},
+	}
+
+	app.HandleDelete("delete 1")
+	actual := app.Store.GetTodos()
+
+	if !slices.Equal(actual, expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestCliHandleEdit(t *testing.T) {
+	store := &storage.Inmemory{Todos: []models.ToDo{
+		{Task: "Task 1", Completed: false},
+		{Task: "Task 2", Completed: false},
+	}}
+	app := App{Store: store}
+
+	expected := []models.ToDo{
+		{Task: "Task 1", Completed: false},
+		{Task: "Edited task", Completed: false},
+	}
+
+	app.HandleEdit("edit 2 Edited task")
+	actual := app.Store.GetTodos()
+
+	if !slices.Equal(actual, expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
